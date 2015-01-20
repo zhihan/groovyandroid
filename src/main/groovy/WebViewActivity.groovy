a19a26769f74f6062176fdd72e795c5a32af309d
Zhi Han <zhi.han@gmail.com>
2015-01-20 15:14:41 -0500

    Added webview evaluateJavascript

--- src/main/groovy/WebViewActivity.groovy
+++ src/main/groovy/WebViewActivity.groovy
@@ -4,7 +4,9 @@ import android.widget.Button
 import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.ValueCallback
 import android.view.View
+import android.util.Log
 
 import groovy.transform.CompileStatic
 
@@ -16,11 +18,26 @@ public class WebViewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_webview);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
 
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://groovy.codehaus.org/");
+        WebView myWebView = (WebView) findViewById(R.id.webview)
+        myWebView.getSettings().setJavaScriptEnabled(true)
+        myWebView.loadData("<html><body><div id=\"c\">C</div></body></html>", 
+            "text/html", "utf-8")
+
+        myWebView.evaluateJavascript(
+            """ function plus(x, y) { 
+                return x + y;
+            }
+            plus(1, 2);
+            """, new ValueCallback<String>(){
+                void onReceiveValue(String x) {
+                    Log.v("WebViewActivity", "Received:" + x)
+                }
+            })
     }
+
+
 }
